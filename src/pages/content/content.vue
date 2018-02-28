073ee85912e7ec244a8fc924a62ec9095ab9b325
coolfxl <18850149410@163.com>
2018-02-28 10:26:19 +0800

    vue框架，囊括vue-router, store, vuex, axios, jquery, echarts, etc,打包可运行

---
+++ src/pages/content/content.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="content">
+    <v-header></v-header>
+    <v-nav></v-nav>
+    <div class="container">
+      <keep-alive>
+        <router-view/>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+import vHeader from 'com/vHeader/vHeader'
+import vNav from 'com/vNav/vNav'
+export default {
+  name: 'content'
+  , data() {
+    return {
+      
+    }
+  }
+  , methods: {
+
+  }
+  , components: {
+    vHeader
+    ,vNav
+  }
+}
+</script>
+
+<style lang="stylus">
+@import '../../assets/css/mixin'
+.container
+  width 100%
+  height 100%
+  padding-top 56px
+  padding-left 120px
+  padding-bottom 30px
+  overflow auto
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,82 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+const login = () =>
+import('pages/login/login')
+const dashboard = () =>
+import('pages/dashboard/dashboard')
+const content = () =>
+import('pages/content/content')
+Vue.use(Router)
+
+let routes = [
+  {
+      path: '/'
+      , redirect: '/login'
+  },
+  {
+      path: '/login',
+      component: login
+  },
+  {
+      path: '/content',
+      component: content,
+      children: [
+        {
+            path: '/dashboard',
+            component: dashboard
+        }, 
+      ]
+  }
+]
+
+routes.forEach((item) => {
+  if (item.path == '/content') {
+      item.children.forEach((route) => {
+          route.meta = {}
+          route.meta.requireAuth = true
+      })
+  }
+})
+
+let router = new Router({
+  routes
+})
+
+// router.beforeEach((to, from, next) => {
+//     if (to.meta.requireAuth) {
+//         if (window.localStorage.getItem('user_id') !== null) {
+//             next()
+//         } else {
+//             next({
+//                 path: '/login',
+//                 query: { redirect: to.fullPath }
+//             })
+//         }
+//     } else {
+//         next()
+//     }
+// })
+
+// router.beforeEach( (to, from, next) => {
+//     if(to.matched.some( record => record.meta.requireAuth)){ // 判断该路由是否需要登录权限 // 还有另一种实现方式 if (to.meta.requireAuth) {
+//         if(window.localStorage.getItem('token') !== null){
+//             if(to.fullPath != ''){
+//                 next()
+//             } else {
+//                 next({
+//                     path: '/basic'
+//                 })
+//             }
+//         } else {
+//             next({
+//                 path: '/login', // 如果没有登录则跳转到登录页面
+//                 query: {redirect: to.fullPath} // 将跳转的路由path作为参数，登录成功后跳转到该路由页面
+//             })
+//         }
+//     } else {
+//         next()
+//     }
+// })
+export default router
